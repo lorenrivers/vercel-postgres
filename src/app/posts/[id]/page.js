a678d1e4da13627e4111cb44b73e9e4347ca7664
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-02-28 10:03:02 +0000

    connecting to the database

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,7 +1,12 @@
-export default function PostIdPage() {
+import { sql } from "@vercel/postgres";
+
+export default async function PostIdPage({ params }) {
+  console.log(params);
+  const post = (await sql`SELECT * FROM posts WHERE id = ${params.id}`).rows[0];
   return (
     <div>
-      <h1>Posts ID</h1>
+      <p>{params.id}</p>
+      <p>{post.content}</p>
     </div>
   );
 }
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,7 +1,16 @@
-export default function PostPage() {
+import { sql } from "@vercel/postgres";
+
+export default async function PostPage() {
+  const posts = await sql`SELECT * FROM posts`;
+
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.rows.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
